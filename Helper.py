71331315f073d1b0f4f512aa3097342ff52e9b4e
brandonwinegar <43932810+brandonwinegar@users.noreply.github.com>
2019-03-12 13:45:20 -0500

    Add files via upload

--- Helper.py
+++ Helper.py
@@ -1,21 +1,35 @@
 import os
 import re
+import inspect
 
 
 def get_io_type(line):
-    return line[0]
+    return line[0].rstrip().strip('\'"')
 
 
 def get_channel_name(line):
-    return line[1]
+    return line[1].rstrip().strip('\'"')
 
 
 def get_channel_key(line):
-    return line[2]
+    return line[2].rstrip().strip('\'"')
 
 
 def get_channel_val(line):
-    return line[3]
+    return line[3].rstrip().strip('\'"')
+
+
+def assemble_prog_id(module_number, channel_number):
+    if (module_number == "") or (channel_number == ""):
+        return ""
+    else:
+        return '{}{}'.format(module_number.rstrip().strip('\'"'), channel_number.strip('\'"'))
+
+
+def process_file(file):
+    pattern = re.compile('(.*)\.(.*)')
+    result = pattern.match(file)
+    return result.group(1)
 
 
 settings = {}
@@ -23,14 +37,11 @@ settings = {}
 
 def process_settings(settings_file):
     # Create a regex pattern to match all text to the left and right of '='s
-    pattern = re.compile(r'(.*)=(.*)\.[A-Za-z]*')
+    pattern = re.compile('(.*)=(.*)')
     with open(settings_file) as file:
         for line in file:
             result = pattern.match(line)
-            try:
-                settings[result.group(1)] = result.group(2)
-            except:
-                pass
+            settings[result.group(1)] = result.group(2)
 
 
 def validate_name(file_name):
--- main.py
+++ main.py
@@ -2,74 +2,85 @@ from Helper import *
 import xlsxwriter
 import re
 import shutil
-from time import gmtime, strftime
+from time import *
+import time
 import os
 
 
-def main(file):
+def main(file_to_convert):
     base_path = os.getcwd()
-    file_path = '{}\\To Convert\\{}.csv'.format(base_path, file)
-    file_name = file + '_generated.xls'
+    file_path = '{}\\To Convert\\{}'.format(base_path, file_to_convert)
+    file_name = process_file(file_to_convert) + '.xls'
     # Opens the selected file and splits elements on a '.' or ',' and organizes the data into lists
     with open(file_path) as f:
         channel_list = list(map(lambda n: re.split('[,.\t]', n), f.readlines()))
         current_channel = get_channel_name(channel_list[0])
-
     #############################################################
     # Creates a master dictionary where each key will           #
     # be a channel name, each value will be a Dict containing   #
     # information  on that specific channel                     #
     #############################################################
     channel_data_dict = {current_channel: {}}
-
     # This loop will assemble data from the old format of CSV into dictionaries to be referenced later
     for currentLine in channel_list:
-        # Execute if reading data from a new channel
         try:
+
+            # Execute if currentLine is from a new channel
             if current_channel != get_channel_name(currentLine):
+                # Execute before moving on to next channel
+                try:
+                    module_number = channel_data_dict[current_channel]['Module']
+                    channel_number = channel_data_dict[current_channel]['Channel']
+                    channel_data_dict[current_channel]['ProgID'] = assemble_prog_id(module_number, channel_number)
+                except:
+                    pass
+                # Moving on to next channel
                 current_channel = get_channel_name(currentLine)
                 channel_data_dict[current_channel] = {}
                 channel_data_dict[current_channel]['Type'] = get_io_type(currentLine)
             channel_data_dict[current_channel][get_channel_key(currentLine)] = get_channel_val(currentLine)
         except:
             pass
-
     # Create the workbook for the data to be written to
-    workbook = xlsxwriter.Workbook(file + '_generated.xls')
+    workbook = xlsxwriter.Workbook(process_file(file_to_convert) + '.xls')
     worksheet = workbook.add_worksheet()
-
     # Standard Headers
     group_names = ['ProgID', 'Tag', 'Detail', 'Type', 'Offset', 'Scaling', 'TaskName', 'Group', 'CalDate', 'Cab Connector']
-
-    # Write Headers to worksheet
+    # Write Headers to worksheet    (row, col, val)
     for i in range(len(group_names)):
         worksheet.write(0, i, group_names[i])
-
     # Write Date to worksheet
     line_number: int = 1
     for key in channel_data_dict:
-        for i in range(len(group_names) - 1):
+        for i in range(len(group_names)):
             try:
-                current_tag = group_names[i+1]
+                current_tag = group_names[i]
                 info = channel_data_dict[key][current_tag].strip()
-                worksheet.write(line_number, i + 1, info)
+                worksheet.write(line_number, i, info)
             except KeyError:
                 # Error will be thrown if no data is present, will write nothing to cell.
-                worksheet.write(line_number, i + 1, '')
+                worksheet.write(line_number, i, '')
         line_number += 1
     workbook.close()
-
     # Place the file into the converted folder and account for any duplicates
     validated_name = validate_name(file_name)
     shutil.move('{}\\{}'.format(base_path, file_name),
                 '{}\\{}\\{}'.format(base_path, 'Converted', validated_name))
-
-    print("\nConversion complete. {} is now in the 'Converted' folder.".format(validated_name))
-    print(strftime('%Y-%m-%d %H:%M:%S', gmtime()))
+    print("{} is now in the 'Converted' folder.".format(validated_name))
 
 
 if __name__ == '__main__':
-        settingsFile = os.getcwd() + '\\settings.txt'.format()
-        process_settings(settingsFile)
-        main(settings['file'])
+    begin_millis = int(round(time.time() * 1000))
+    settingsFile = os.getcwd() + '\\settings.txt'.format()
+    process_settings(settingsFile)
+    print()
+    if settings['DoAll']:
+        file_list = settings['FileList'].split(',')
+        for file in file_list:
+            main(file)
+    else:
+        main(settings['SingleFile'])
+    end_millis = int(round(time.time() * 1000))
+    print('\nConversion took {} seconds'.format((end_millis-begin_millis)/1000))
+    print(strftime('%Y-%m-%d %H:%M:%S', gmtime()))
 
